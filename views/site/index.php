f12aa4d6798ae399173865f1a92cdfe50d7791a2
Sathit Seethaphon <dixonsatit@gmail.com>
2015-03-11 20:41:27 +0700

    edit site index

--- views/site/index.php
+++ views/site/index.php
@@ -1,39 +1,41 @@
 <?php
 /* @var $this yii\web\View */
 $this->title = 'My Yii Application';
+use yii\helpers\Html;
 ?>
 <div class="site-index">
 
     <div class="jumbotron">
-        <h1>Congratulations!</h1>
+        <h1>Yii 2 Learning</h1>
 
         <p class="lead">You have successfully created your Yii-powered application.</p>
 
-        <p><a class="btn btn-lg btn-success" href="http://www.yiiframework.com">Get started with Yii</a></p>
+        <p><a class="btn btn-lg btn-success" href="https://github.com/dimpled/Yii2-Learning">Tutorial</a>
+        <a class="btn btn-lg btn-success" href="https://github.com/dimpled/Yii2-Learning-Source">Download</a></p>
     </div>
 
     <div class="body-content">
 
         <div class="row">
             <div class="col-lg-4">
-                <h2>Heading</h2>
+                <h2>Getting Started</h2>
 
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
+                <ul>
+                    <li><a href="https://github.com/dimpled/Yii2-Learning/blob/master/Chapter%201/Installation.md">Install</a></li>
+                </ul>
 
-                <p><a class="btn btn-default" href="http://www.yiiframework.com/doc/">Yii Documentation &raquo;</a></p>
+               
             </div>
             <div class="col-lg-4">
-                <h2>Heading</h2>
+                <h2>Widget</h2>
 
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
+               <ul>
+                   <li>
+                       <?= Html::a('การติดตั้ง GridView + ExportMenu',['countries/index']); ?>
+                   </li>
+               </ul>
 
-                <p><a class="btn btn-default" href="http://www.yiiframework.com/forum/">Yii Forum &raquo;</a></p>
+                
             </div>
             <div class="col-lg-4">
                 <h2>Heading</h2>
